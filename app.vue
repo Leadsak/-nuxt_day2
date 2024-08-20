1ef0bf8d5d25390e432fd17c863c7860d0c99f94
Leadsak <zam210641@gmail.com>
2024-08-20 16:30:30 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,43 @@
+<template>
+    
+    <div class="bg-teal-50 w-full h-screen flex items-center justify-center gap-5">
+        <div class="bg-rose-500 h-[500px] w-[500px] grid  grid-rows-2  border-4 border-gray-950 text-[30px] text-neutral-50  rounded-lg  hover:scale-90 transition-all delay-200 " >
+            <div class="items-center flex justify-center  ">01</div>
+            <div class="bg-blue-500  items-center flex justify-center ">02</div>
+        </div>
+
+        <div class="bg-rose-500 h-[500px] w-[500px] grid  grid-cols-2 border-4 border-gray-950 text-[30px] text-neutral-50  rounded-lg animate-pulse   ">
+            <div class="bg-blue-500 items-center flex justify-center " >01</div>
+            <div class="text-center items-center flex justify-center ">02</div>
+        </div>
+
+        <div class="bg-rose-500 h-[500px] w-[500px] grid grid-cols-3 border-4 border-gray-950  text-[30px] text-neutral-50 rounded-lg animate-bounce  ">
+            <div class="bg-blue-500 items-center flex justify-center">01</div>
+            <div class="items-center flex justify-center">02</div>
+            <div class="bg-blue-500 items-center flex justify-center ">03</div>
+            
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+        <!-- <div class="bg-sky-500 w-[500px] h-[500px] grid grid-row-2 ">
+            <div class="bg-rose-500">01</div>
+            <div class="bg-blue-500">02</div>
+        </div>
+        <div class="bg-violet-500 w-[500px] h-[500px] grid grid-cols-3">
+            <div class="bg-blue-500">01</div>
+            <div class="bg-rose-500">02</div>
+            <div class="bg-blue-500">03</div>
+        </div> -->
+
+    
+  </template>
